50cb00dd970f72105e3ae5b7c0e4ebfbf7fb5998
JongChan Choi <chan@spoqa.com>
2017-09-27 18:16:31 +0900

    0.0.9

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,13 @@
+function is(a: any, b: any): boolean {
+    if (a === b) {
+        return (a !== 0) || ((1 / a) === (1 / b));
+    } else {
+       return (a !== a) && (b !== b);
+    }
+}
+
 export function shallowEqual(objA: any, objB: any): boolean {
-    if (Object.is(objA, objB)) return true;
+    if (is(objA, objB)) return true;
     if (typeof objA !== 'object' || objA === null ||
         typeof objB !== 'object' || objB === null) return false;
     const keysA = Object.keys(objA);
@@ -7,7 +15,7 @@ export function shallowEqual(objA: any, objB: any): boolean {
     if (keysA.length !== keysB.length) return false;
     for (let key of keysA) {
         if (!objB.hasOwnProperty(key)) return false;
-        if (!Object.is(objA[key], objB[key])) return false;
+        if (!is(objA[key], objB[key])) return false;
     }
     return true;
 }
